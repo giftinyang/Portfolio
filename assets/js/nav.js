a83f172a32dbfd10132a1b7ac8545adfb73225e2
nedyudombat <nedyudombat@gmail.com>
2019-02-11 22:29:15 +0100

    Implement responsive header

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+// navbar responsiveness
+const navbarResponsiveness = () => {
+  const x = document.getElementById('myTopnav');
+  if (x.className === 'topnav') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'topnav';
+  }
+};
